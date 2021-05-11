70690f46a9dbdf72923286a14ebf0934c19c7f8e
squatcub <brandon.alexis10@hotmail.com>
2021-05-10 22:51:39 -0500

    Primer commit

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Brandon Rodriguez Molina on 06/05/21.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
